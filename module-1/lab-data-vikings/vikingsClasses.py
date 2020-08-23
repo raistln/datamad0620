3eadfb809628cbd685e087548e89bbd4eb36fdf7
Sam Mar <samumarfon@gmail.com>
2020-08-23 16:12:26 +0200

    Resuelto el lab de vikingos

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,4 +1,4 @@
-
+import random
 # Soldier
 
 
@@ -8,7 +8,6 @@ class Soldier:
         self.strength = strength
     def attack(self):
         return self.strength
-
     def receiveDamage(self, damage):
         self.health -= damage
 
@@ -17,75 +16,69 @@ class Soldier:
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        self.name = name
+        #self.name = name
         super().__init__(health, strength)
-    def attack(self):
-        return Soldier.attack(self)
+        self.name = name #CORRECCIÓN: Primero los atributos heredados, luego los propios, por si hay que sobreescribir alguno
+
     def receiveDamage(self, damage):
-        Soldier.receiveDamage(self, damage)
-        if self.health <= 0:
+        #self.health -= damage
+        super().receiveDamage(damage) #CORRECCIÓN: Aunque cambiemos una función heredada, aún se puede usar la función padre
+        if self.health < 1:
             return f"{self.name} has died in act of combat"
-        if self.health > 0:
+        else:
             return f"{self.name} has received {damage} points of damage"
     def battleCry(self):
         return "Odin Owns You All!"
-
 
 # Saxon
 
 
 class Saxon(Soldier):
-    def __init__(self, health, strength):
-        super().__init__(health, strength)
-    def attack(self):
-        return Soldier.attack(self)
+    # def __init__(self, health, strength):
+    #    super().__init__(health, strength) #CORRECCIÓN: No hay que iniciar saxon porque es igual que la clase padre
+
     def receiveDamage(self, damage):
-        self.health -= damage
-        if self.health <= 0:
+        #    self.health -= damage
+        super().receiveDamage(damage)  # CORRECCIÓN: Igual que en la clase vikings
+        if self.health < 1:
             return f"A Saxon has died in combat"
-        if self.health > 0:
+        else:
             return f"A Saxon has received {damage} points of damage"
-# War
+
+        # War
 
 
 class War:
-
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
+
     def addViking(self, Viking):
         self.vikingArmy.append(Viking)
+
     def addSaxon(self, Saxon):
         self.saxonArmy.append(Saxon)
+
     def vikingAttack(self):
-        import random
-        sax = random.choice(self.saxonArmy)
-        vik = random.choice(self.vikingArmy)
-        damage = vik.attack()
-        sax.receiveDamage(damage)
-        if sax.health <= 0:
-            self.saxonArmy.remove(sax)
-        else:
-            return sax.receiveDamage(damage)
+        saxon = random.choice(self.saxonArmy)
+        viking_damage = random.choice(self.vikingArmy)
+        result = saxon.receiveDamage(viking_damage.attack())
+        if saxon.health < 1:
+            self.saxonArmy = [e for e in self.saxonArmy if e.health > 0]
+        return result
 
     def saxonAttack(self):
-        import random
-        sax = random.choice(self.saxonArmy)
-        vik = random.choice(self.vikingArmy)
-        damage = sax.attack()
-        vik.receiveDamage(damage)
-        if vik.health <= 0:
-            self.vikingArmy.remove(vik)
-        else:
-            return vik.receiveDamage(damage)
-
+        viking = random.choice(self.vikingArmy)
+        saxon_damage = random.choice(self.saxonArmy)
+        result = viking.receiveDamage(saxon_damage.attack())
+        if viking.health < 1:
+            self.vikingArmy = [e for e in self.vikingArmy if e.health > 0]
+        return result
 
     def showStatus(self):
-            v_size = len(self.vikingArmy)
-            s_size = len(self.saxonArmy)
-            if v_size == 0:
-                return f"Saxons have fought for their lives and survive another day..."
-            if s_size == 0:
-                return f"Vikings have won the war of the century!"
-            if v_size != 0 and s_size != 0:
-                return f"Vikings and Saxons are still in the thick of battle."
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
